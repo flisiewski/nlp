58d5698f2f4823c717762c86bf6e9d58c252ad5b
Maciej Rapacz <maciek.rapacz@gmail.com>
2018-03-10 18:58:39 +0100

    Add first sketch of cluster loading

--- lab2/main.py
+++ lab2/main.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+from tqdm import tqdm
 from elasticsearch_dsl import analyzer, DocType, Text, Date, Keyword, Nested, InnerDoc
 
 analyzer(
@@ -8,8 +12,7 @@ analyzer(
 
 
 class Judge(InnerDoc):
-    first_name = Text(analyzer=analyzer)
-    last_name = Text(analyzer=analyzer)
+    name = Text(analyzer=analyzer)
 
 
 class Judgment(DocType):
@@ -17,3 +20,36 @@ class Judgment(DocType):
     judgment_date = Date()
     signature = Keyword()
     judges = Nested(Judge)
+
+
+Judge.init()
+Judgment.init()
+
+DATA_DIR = "/run/media/maciej/Nowy/data/json/"
+CHOSEN_YEAR = str(2011)
+
+
+def load_data():
+    files = os.listdir(DATA_DIR)
+    results = []
+    for file in tqdm(files):
+        if file.startswith("judgment"):
+            file_path = os.path.join(DATA_DIR, file)
+
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+                judgments = [x for x in data["items"] if x["judgmentDate"].startswith(CHOSEN_YEAR)]
+
+            for judgment in judgments:
+                Judgment(
+                    content=judgment['textContent'],
+                    judgment_date=judgment['judgmentDate'],
+                    signature=judgment['id'],
+                    judge=[Judge(name=judge['name']) for judge in judgment['judges']],
+                )
+                Judgment.save()
+
+    return results
+
+
+load_data()
